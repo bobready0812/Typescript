a1b9a4b51287cc595a13c36668a3fba6a9b583a6
DaehyeonKim <Daehyeonoff@gmail.com>
2021-09-01 21:33:59 +0900

    20210901

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-function logName(name) {
-    console.log(name);
-}
-logName('Jack');
+var user1 = {
+    id: 1,
+    name: "plus",
+    age: 20,
+    isgood: true,
+};
+console.log(user1.id);
+console.log(user1.name);
+console.log(user1.age);
+console.log(user1.isgood);
--- app.ts
+++ app.ts
@@ -2,18 +2,22 @@ interface User {
     id:number;
     name:string;
     age:number;
+    isgood:boolean;
 }
 
 
-const user1: User = {
+const User1: User = {
     id: 1 ,
     name:"plus",
     age : 20,
+    isgood:true,
 }
 
 
 console.log(user1.id);
-
+console.log(user1.name);
+console.log(user1.age);
+console.log(user1.isgood);
 
 
 

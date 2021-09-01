a01d2062fb7372bc4fd8837762852758752acbea
DaehyeonKim <Daehyeonoff@gmail.com>
2021-09-01 21:31:21 +0900

    first commit

---
+++ app.ts
@@ -0,0 +1,19 @@
+interface User {
+    id:number;
+    name:string;
+    age:number;
+}
+
+
+const user1: User = {
+    id: 1 ,
+    name:"plus",
+    age : 20,
+}
+
+
+console.log(user1.id);
+
+
+
+
